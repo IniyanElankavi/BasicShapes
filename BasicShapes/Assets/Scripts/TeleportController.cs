7dcd03d529b33600cb436297b039a0d2aa21523a
Iniyan <iniyane@ggsinc.com>
2022-03-21 15:40:18 +0530

    Environment Setup and Teleportation Added

---
+++ BasicShapes/Assets/Scripts/TeleportController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleportController : MonoBehaviour
+{
+    public GameObject Player;
+    float timeLeft = 1.0f;
+    public bool isPointerIN = false;
+
+    public void PointerIn()
+    {
+        isPointerIN = true;
+    }
+
+    public void PointerOut()
+    {
+        isPointerIN = false;
+    }
+
+    private void Update()
+    {
+        if (isPointerIN)
+        {
+            timeLeft -= Time.deltaTime;
+            if (timeLeft < 0)
+            {
+                Player.transform.position = new Vector3(transform.position.x, transform.position.y + 1.3f, transform.position.z);
+            }
+        }
+        else
+        {
+            timeLeft = 1.0f;
+        }
+    }
+}
